e4f5ad19c8b563f5c19c2b20d4206e25e776076e
Epic david <toonerdrill@gmail.com>
2020-05-21 10:26:50 -0700

    Auto pouplated user field on Registration serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from rest_auth.registration.serializers import RegisterSerializer
 from .models import User
-
+from allauth.account.adapter import get_adapter
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,9 +9,27 @@ class UserSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class SignupSerializer(RegisterSerializer):
-    is_client = serializers.BooleanField()
-    is_staff = serializers.BooleanField()
+    btc_wallet = serializers.CharField(max_length=300)    
 
     class Meta:
         model = User
-        fields = '__all__'      
+        fields = ('email','username','password','btc_wallet') 
+
+
+    def get_cleaned_data(self):
+        return {
+            'username': self.validated_data.get('username', ''),
+            'password1': self.validated_data.get('password1', ''),
+            'password2': self.validated_data.get('password2', ''),
+            'btc_wallet': self.validated_data.get('btc_wallet', ''),
+            'email': self.validated_data.get('email', '')
+        }
+
+    def save(self, request):
+        adapter = get_adapter()
+        user = adapter.new_user(request)
+        self.cleaned_data = self.get_cleaned_data()
+        user.is_student = True
+        adapter.save_user(request, user, self)
+        return user
+    
